f2835aebb96f8189b976d5b4220a12855ca480e5
nale6 <alejcaz6@gmail.com>
2024-12-02 21:06:55 -0500

    Defined vertex class with edge subclass

--- Vertex.java
+++ Vertex.java
@@ -1,3 +1,30 @@
+import java.util.List;
+
 public class Vertex {
-  
+
+  public class Edge{
+    public Vertex originVertex, destinationVertex;
+    public Integer weight;
+
+    public Edge(Vertex originVertex, Vertex destinationVertex){
+      this.originVertex = originVertex;
+      this.destinationVertex = destinationVertex;
+    }
+  }
+
+  private String label;
+  List<Edge> edges;
+
+  public Vertex(String label){
+    this.label = label;
+  }
+
+  public String getLabel(){
+    return label;
+  }
+
+  @Override
+  public String toString(){
+    return this.label;
+  }
 }
